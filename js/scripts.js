4f9871dac491a28b9e8c5fd8b1098eed49622414
Usuario Local <olesya.suro@gmail.com>
2021-11-02 09:40:43 +0100

    Creado js. Realizados ejercicios para alumnos
    Inicializado ejercicio js

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//Ejercicio 1
+
+let nombrePila = "Olesya";
+console.log(nombrePila);
+
+
+//Ejercicio 2
+// nombrePila = "Ole";
+// console.log(nombrePila)
+
+//Ejercicio 3
+//Me dice si comento la variable que no está definida
+
+let operador1=3;
+let operador2=5;
+// console.log(operador1+operador2)
+let resultado=0;
+
+resultado = operador1+operador2;
+console.log(resultado)
+
+operador1=67;
+
+
+
+
+
+
+
